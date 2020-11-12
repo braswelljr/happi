8d42dd3a9d290b7874e715daa956f69d3cde4cf8
braswelljr <braswellkenneth7@gmail.com>
2020-11-12 20:32:43 +0000

    Setup config

---
+++ lib/server.ts
@@ -0,0 +1,11 @@
+const http = require('http');
+const app = require('./app');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const hostname = process.env.HOST || '127.0.0.1' || 'localhost';
+
+const server = http.createServer(app);
+
+server.listen(PORT, hostname, () => console.log(`Server started at http://${hostname}:${PORT}`));
